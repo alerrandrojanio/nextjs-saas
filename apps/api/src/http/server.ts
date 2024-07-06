98389f667640d1604dbdc65616395c9bac12a44b
Alerrandro Janio <alerrandro.janio07@gmail.com>
2024-07-06 20:35:09 -0300

    feat: database and create user route

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,29 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  // jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { createAccount } from './routes/auth/create-account'
+
+const PORT = 3333
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.register(createAccount)
+
+app
+  .listen({
+    port: PORT,
+  })
+  .then(() => {
+    console.log(`Server is running on port ${PORT} 🚀`)
+  })
